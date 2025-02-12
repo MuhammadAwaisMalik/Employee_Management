d82ba9f32d65882bf279853f92699dd75ca9b0a6
Muhammad Awais <mawaismalik520@gmail.com>
2025-02-12 18:37:52 +0500

    update fe url

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://employee-management-theta-gilt.vercel.app"],
     credentials: true,
   })
 );
