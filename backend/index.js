96a36b38e62929f9c5f8fb81e379200cdcc6ff53
Muhammad Awais <mawaismalik520@gmail.com>
2025-02-12 20:20:44 +0500

    udpate for cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: "https://employee-management-5gzm.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
